70a5dbcb5c53300ab6401a0edc56675d16b5e10d
chongyang-xu <giantpoplar@163.com>
2018-05-28 11:13:47 +0800

    add interval accumulate timer

--- dev_util/test_timing.cpp
+++ dev_util/test_timing.cpp
@@ -11,13 +11,17 @@ int cc[L][L];
 int main(){
 
 	TIK(FOR_i_j_k);
+	TIMER(ijk_acc);
 	for(int i=0;i<L;i++){
+		if(i%2==0)PICK_TIK(ijk_acc);
 		for(int j=0;j<L;j++){
 			for(int k=0;k<L;k++){
 				c[i][j] += a[i][k] * b[k][j];
 			}
 		}
+		if(i%2==0)PICK_TOK(ijk_acc);
 	}
+	TIMESUM(ijk_acc);
 	TOK(FOR_i_j_k);
 
 	TIK(FOR_i_k_j);
@@ -29,8 +33,6 @@ int main(){
 		}
 	}
 	TOK(FOR_i_k_j);
-
-	
 
 	return 0;
 }
